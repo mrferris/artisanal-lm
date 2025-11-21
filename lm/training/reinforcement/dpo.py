df8517edba46f652d39834c28c20ad2cc2249c30
Michael Ferris <mrferris@buffalo.edu>
2025-11-21 12:11:12 -0500

    Implements calculation of logprobs of prompt response pairs for a given model

--- lm/training/reinforcement/dpo.py
+++ lm/training/reinforcement/dpo.py
@@ -1,5 +1,42 @@
 import torch
-from jaxtyping import Float
+from jaxtyping import Float, Int
+
+
+def calculate_model_log_probs(
+    model: torch.nn.Module,
+    prompt_token_sequence: Int[torch.Tensor, "... batch_size seq_len"],
+    prompt_lengths: Int[torch.Tensor, "... batch_size"],
+    output_token_sequence: Int[torch.Tensor, "... batch_size seq_len"],
+    output_length: Int[torch.Tensor, "... batch_size"],
+) -> Float[torch.Tensor, "... batch_size"]:
+    """
+    Calculates the probabilities in logspace of getting a response from a model, given a prompt.
+    Args:
+        model: instance of model whose log_probablities given prompt+output we are calculating.
+    """
+    full_token_sequences = torch.cat((prompt_token_sequence, output_token_sequence), dim=-1)
+    positions = torch.arange(full_token_sequences.shape[-1] - 1, device=model.device)
+
+    # Get the logged probabliities, throwing away the final one.
+    output = model(full_token_sequences)
+    log_probs = torch.log_softmax(output, dim=-1)
+    log_probs = log_probs[:, :-1, :]
+
+    # Remove first token ID, we don't predict it
+    targets = full_token_sequences[:, 1:]
+    gathered_log_probs = torch.gather(log_probs, dim=-1, index=targets.unsqueeze(-1)).squeeze(-1)
+
+    # We only want to add up starting at the first logit predicting the response.
+    mask_start = (prompt_lengths - 1).unsqueeze(-1)
+    mask_end = (prompt_lengths - 1 + output_length).unsqueeze(-1)
+    mask = (positions >= mask_start) & (positions < mask_end)
+
+    masked_log_probs = gathered_log_probs * mask
+    summed_log_probs = torch.sum(masked_log_probs, dim=-1)
+
+    print(torch.exp(summed_log_probs))
+
+    return summed_log_probs
 
 
 def calculate_dpo_loss(
