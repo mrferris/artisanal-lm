8bf546261f3f1d37f019415458904bef818306ab
Michael Ferris <mrferris@buffalo.edu>
2025-11-21 17:26:26 -0500

    Adds comments and fixes formatting

--- lm/training/utils/gradient_clipping.py
+++ lm/training/utils/gradient_clipping.py
@@ -1,12 +1,17 @@
-import torch
 import math
-def clip_gradients(params: list[torch.nn.Parameter], max_l2_norm: float, eps=1e-6) -> None:
 
+import torch
+
+
+def clip_gradients(params: list[torch.nn.Parameter], max_l2_norm: float, eps=1e-6) -> None:
+    """
+    Implements clipping of L2 norm of gradients.
+    """
     summed_grad_norm = 0.0
     for param in params:
         if param.grad is not None:
             summed_grad_norm += param.grad.norm(2) ** 2
-        
+
     l2_norm = math.sqrt(summed_grad_norm)
     if l2_norm > max_l2_norm:
         scaling = max_l2_norm / (l2_norm + eps)
--- lm/training/utils/scheduler.py
+++ lm/training/utils/scheduler.py
@@ -1,15 +1,16 @@
 import math
 
+
 def learning_rate_scheduler(current_step: int, max_rate: float, min_rate: float, warmup_iterations: int, cosine_annealing_iterations: int) -> float:
-
+    """
+    Implements cosine-annealed learning rate schedule.
+    """
     if current_step < warmup_iterations:
-
         return (current_step / warmup_iterations) * max_rate
-    
-    if current_step > cosine_annealing_iterations:
 
+    if current_step > cosine_annealing_iterations:
         return min_rate
-    
+
     proportion_of_annealing_complete = (current_step - warmup_iterations) / (cosine_annealing_iterations - warmup_iterations)
 
     cos_annealing_term = 1 + math.cos(proportion_of_annealing_complete * math.pi)
