a3d4b869bbd99847d8ee9e0875f67caef7b8ccdb
Michael Ferris <mrferris@buffalo.edu>
2025-11-25 15:22:08 -0500

    Adds DPO step implementation with TrainableModel wrapper

--- lm/model/model.py
+++ lm/model/model.py
@@ -5,7 +5,8 @@ from jaxtyping import Float, Int
 from lm.model.components.attention import Rope
 from lm.model.components.linear import Embedding, Linear, RMSNorm
 from lm.model.components.transformer import Transformer
-from lm.training.reinforcement.dpo import calculate_model_logprobs
+from lm.training.optimization.adamw import AdamW
+from lm.training.reinforcement.dpo import calculate_model_log_probs, calculate_simpo_loss
 from lm.training.utils.checkpointing import load_checkpoint
 
 
@@ -90,7 +91,87 @@ class TransformerLM(nn.Module):
         return [num_parameters, non_embedding_parameters]
 
     def load_checkpoint(self, checkpoint: str):
+        """
+        Load a model for inference with no optimizer state.
+        """
         load_checkpoint(checkpoint, self, None)
 
-    def do_simpo_step(self, prompt, positive, negative):
-        calculate_model_logprobs(self, prompt, prompt_lengths, output)
+
+class TrainableModel:
+    """
+    A model wrapped in a bunch of training utilities to enable continuous learning.
+    """
+
+    def __init__(self, model: nn.Module):
+        self.model = model
+
+        self.optimizer = AdamW(
+            model.parameters(),
+            lr=5e-4,
+            betas=[0.9, 0.95],
+            eps=1e-4,
+            weight_decay=0.01,
+        )
+
+    def do_simpo_step(
+        self,
+        prompt: list[int],
+        positive: list[int],
+        negative: list[int],
+    ):
+        """
+        Executes SimPO training on a single (prompt, positive, negative) triple.
+        Aligns the model to respond more like the positive response example,
+        and less like the negative.
+        Args:
+            prompt: List of token IDs for the prompt
+            positive: List of token IDs for the positive
+            negative: List of token IDs for the negative
+        """
+        # calculate_log_probs expects the same number of prompts in the batch dimension as responses.
+        # We will stack positive on top of negative, for outputs and output lengths.
+        prompt_tensor = torch.tensor(prompt, dtype=torch.int).expand(2, -1).to(self.model.device)
+        prompt_length_tensor = torch.Tensor([len(prompt), len(prompt)]).to(self.model.device)
+
+        response_tensor = torch.stack(
+            [
+                torch.tensor(positive, dtype=torch.int),
+                torch.tensor(negative, dtype=torch.int),
+            ],
+        ).to(self.model.device)
+        response_length_tensor = torch.tensor(
+            [len(positive), len(negative)],
+            dtype=torch.int,
+        ).to(self.model.device)
+
+        log_probs = calculate_model_log_probs(
+            self.model,
+            prompt_token_sequence=prompt_tensor,
+            prompt_lengths=prompt_length_tensor,
+            output_token_sequence=response_tensor,
+            output_length=response_length_tensor,
+        )
+
+        loss = calculate_simpo_loss(
+            policy_positive_log_prob=log_probs[0],
+            policy_negative_log_prob=log_probs[1],
+            positive_length=prompt_length_tensor[0],
+            negative_length=prompt_length_tensor[1],
+        )
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        after_log_probs = calculate_model_log_probs(
+            self.model,
+            prompt_token_sequence=prompt_tensor,
+            prompt_lengths=prompt_length_tensor,
+            output_token_sequence=response_tensor,
+            output_length=response_length_tensor,
+        )
+
+        before_probs = torch.exp(log_probs)
+        after_probs = torch.exp(after_log_probs)
+
+        print(f"Probabilities before: {before_probs}")
+        print(f"Probabilities after: {after_probs}")
