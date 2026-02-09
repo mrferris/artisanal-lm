1c337b74c4c628fd2e84521220efc43a76e88c09
Michael Ferris <mrferris@buffalo.edu>
2026-02-09 10:36:40 -0500

    fix: loading checkpoint trained on GPU on CPU-only pytorch requires device flag

--- lm/training/utils/checkpointing.py
+++ lm/training/utils/checkpointing.py
@@ -6,15 +6,25 @@ import torch.nn as nn
 import torch.optim as optim
 
 
-def save_checkpoint(model: nn.Module, optimizer: optim.Optimizer, iteration: int, out: str | os.PathLike | typing.BinaryIO | typing.IO[bytes]):
+def save_checkpoint(
+    model: nn.Module,
+    optimizer: optim.Optimizer,
+    iteration: int,
+    out: str | os.PathLike | typing.BinaryIO | typing.IO[bytes],
+):
     state = {"model": model.state_dict(), "optimizer": optimizer.state_dict(), "iteration": iteration}
     torch.save(state, out)
 
     return
 
 
-def load_checkpoint(src: str | os.PathLike | typing.BinaryIO | typing.IO[bytes], model: nn.Module, optimizer: optim.Optimizer | None = None) -> int:
-    state = torch.load(src, weights_only=False)
+def load_checkpoint(
+    src: str | os.PathLike | typing.BinaryIO | typing.IO[bytes],
+    model: nn.Module,
+    optimizer: optim.Optimizer | None = None,
+    device: torch.device | None = None,
+) -> int:
+    state = torch.load(src, weights_only=False, map_location=device)
     model.load_state_dict(state["model"])
     if optimizer is not None:
         optimizer.load_state_dict(state["optimizer"])
